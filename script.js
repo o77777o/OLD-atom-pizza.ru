7693517653775f77c862eaf8a8b1010ccf8ecb51
Mikel <136020078+o77777o@users.noreply.github.com>
2023-12-04 00:56:40 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+document.querySelector(".stt").onclick = scrollToTopBtn;
+function scrollToTopBtn() {
+  scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+const scrollToTopEL = document.querySelector(".stt");
+document.addEventListener("scroll", () => {
+  window.scrollY >= 165
+    ? scrollToTopEL.classList.remove("upward")
+    : scrollToTopEL.classList.add("upward");
+});
+
+const InvisibleLoadingEL = document.querySelector(".stt.upward");
+document.addEventListener("scroll", () => {
+  if (window.scrollY >= 150) {
+    InvisibleLoadingEL.removeAttribute("style");
+  }
+});
+
+const HeadGroupEL = document.querySelector(".HeadGroup");
+document.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  window.scrollY >= 430
+    ? (HeadGroupEL.classList.add = "fixed")
+    : (HeadGroupEL.classList.remove = "static");
+});
+
---
+++ uuidv.js
@@ -0,0 +1,10 @@
+const pizzas = require("./pizzas");
+const { uuid } = require("uuidv4");
+
+for (let i = 0; i < pizzas.length; i++) {
+    console.log(uuid())
+}
+
+const CreatPizzaID = () => {
+    
+}
