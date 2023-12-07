7b93b9a0093446b6b2386e3ae5a65cd25a8052df
Mikel <136020078+o77777o@users.noreply.github.com>
2023-12-08 00:13:01 +0300

    ...

--- uuidv.js
+++ uuidv.js
@@ -1,7 +1,8 @@
-const pizzas = require("./pizzas");
-const { uuid } = require("uuidv4");
+// const pizzas = require("./pizzas");
+// const { uuid } = require("uuidv4");
 
-for (let i = 0; i < pizzas.length; i++) {
-    console.log(uuid())
-}
+// for (let i = 0; i < pizzas.length; i++) {
+//     console.log(uuid())
+// }
 
+const sortedPizzas = pizzas.slice().sort((a, b) => (a.name > b.name) ? 1 : -1);
