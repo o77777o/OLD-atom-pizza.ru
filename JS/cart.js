04eb3f941f1b178673bbe0e99b97fcef3291811a
Ivan Efremov <iefremov@laoban.org>
2023-12-20 20:31:20 +0300

    Update]

--- JS/cart.js
+++ JS/cart.js
@@ -1,84 +1,97 @@
-let selectedProduct = "";
-let productNumber = 0;
+// let selectedProduct = "";
+// let productNumber = 0;
 
-const DB_NAME = "DataBase";
-if (!localStorage.getItem(DB_NAME)) {
-  localStorage.setItem(DB_NAME, JSON.stringify([]));
-}
-let cart = JSON.parse(localStorage.getItem(DB_NAME));
+const DB_NAME = 'DataBase';
 
-const allButtons = document.querySelectorAll(
-  ".price_product_card, .price_modal_card"
-);
-const totalProduct = document.querySelector(".total_product");
-//----------------------------------------------
+// let cart = JSON.parse(localStorage.getItem(DB_NAME));
 
-const getCartFromLS = (i) => {
-  productNumber += 1;
+// const allButtons = document.querySelectorAll(
+//   ".price_product_card"
+// );
+// const totalProduct = document.querySelector(".total_product");
+// //----------------------------------------------
 
-  const productInCart = document.createElement("div");
-  productInCart.classList.add("product");
-  productInCart.innerHTML = `
-    <div class="product_number">${productNumber}.</div>
-    <div class="product_name">${cart[i].name}</div>
-    <div class="product_count">3 шт.</div>
-    <div class="product_price">${cart[i].price}₽</div>
-    `;
-  totalProduct.appendChild(productInCart);
+// const getCartFromLS = (i) => {
+//   productNumber += 1;
+
+//   const productInCart = document.createElement("div");
+//   productInCart.classList.add("product");
+//   productInCart.innerHTML = `
+//     <div class="product_number">${productNumber}.</div>
+//     <div class="product_name">${cart[i].name}</div>
+//     <div class="product_count">3 шт.</div>
+//     <div class="product_price">${cart[i].price}₽</div>
+//     `;
+//   totalProduct.appendChild(productInCart);
+// };
+
+// const displayProductToCart = (i) => {
+//   productNumber += 1;
+//   const correctIndex = i % menuPosition.length;
+//   const productInCart = document.createElement("div");
+//   productInCart.classList.add("product");
+//   productInCart.innerHTML = `
+//     <div class="product_number">${productNumber}.</div>
+//     <div class="product_name">${menuPosition[correctIndex].name}</div>
+//     <div class="product_count">3 шт.</div>
+//     <div class="product_price">${menuPosition[correctIndex].price}₽</div>
+//     `;
+//   totalProduct.appendChild(productInCart);
+// };
+
+// const saveProductToLS = (i) => {
+//   const correctIndex = i % menuPosition.length;
+//   console.log(menuPosition, menuPosition.length, correctIndex);
+//   selectedProduct = menuPosition[correctIndex];
+
+//   cart = JSON.parse(localStorage.getItem(DB_NAME));
+//   if (!localStorage.getItem(DB_NAME)) {
+//     localStorage.setItem(DB_NAME, JSON.stringify([]));
+//   }
+//   cart.push(selectedProduct);
+//   localStorage.setItem(DB_NAME, JSON.stringify(cart));
+// };
+
+// const clearAllCart = () => {
+//   productNumber = 0;
+//   window.localStorage.setItem(DB_NAME, JSON.stringify([]));
+
+//   let productToDelete = document.querySelectorAll(".product");
+//   productToDelete.forEach((i) => {
+//     i.remove();
+//   });
+// };
+
+const processButton = (id) => {
+  const element = menuPosition.find((element) => element.ID === id);
+
+  if (!element) {
+    alert('Ты куда жмал бля');
+    return;
+  }
+
+  const all = JSON.parse(localStorage.getItem(DB_NAME));
+
+  localStorage.setItem(DB_NAME, JSON.stringify([...all, element]));
 };
 
-const displayProductToCart = (i) => {
-  productNumber += 1;
-  const correctIndex = i % menuPosition.length;
-  const productInCart = document.createElement("div");
-  productInCart.classList.add("product");
-  productInCart.innerHTML = `
-    <div class="product_number">${productNumber}.</div>
-    <div class="product_name">${menuPosition[correctIndex].name}</div>
-    <div class="product_count">3 шт.</div>
-    <div class="product_price">${menuPosition[correctIndex].price}₽</div>
-    `;
-  totalProduct.appendChild(productInCart);
-};
-
-const saveProductToLS = (i) => {
-  const correctIndex = i % menuPosition.length;
-  selectedProduct = menuPosition[correctIndex];
-
-  cart = JSON.parse(localStorage.getItem(DB_NAME));
+const init = () => {
   if (!localStorage.getItem(DB_NAME)) {
     localStorage.setItem(DB_NAME, JSON.stringify([]));
   }
-  cart.push(selectedProduct);
-  localStorage.setItem(DB_NAME, JSON.stringify(cart));
+
+  const products = document.querySelectorAll('.product_card');
+
+  for (const product of products) {
+    const description = product.childNodes[2];
+    const addToCartButton = description.children[2];
+
+    addToCartButton.addEventListener('click', () => {
+      processButton(product.id);
+    });
+  }
 };
 
-for (let i = 0; i < cart.length; i++) {
-  getCartFromLS(i);
-}
-
-for (let i = 0; i < allButtons.length; i++) {
-  allButtons[i].addEventListener("click", () => {
-    saveProductToLS(i);
-    displayProductToCart(i);
-  });
-}
-
-const ClearAllCart = () => {
-  productNumber = 0;
-  window.localStorage.setItem(DB_NAME, JSON.stringify([]));
-
-  let productToDelete = document.querySelectorAll(".product");
-  productToDelete = Array.from(productToDelete);
-  productToDelete.forEach((i) => {
-    i.remove();
-  });
-
-  //
-  console.clear();
-  console.log("Local Storage was cleared");
+window.onload = () => {
+  init();
 };
-
-const deleteBtn = document.querySelector(".button_clear_all");
-
-deleteBtn.onclick = ClearAllCart;
--- JS/create_modal.js
+++ JS/create_modal.js
@@ -1,21 +1,21 @@
-const modalMenu = document.querySelector(".modal_menu");
+// const modalMenu = document.querySelector(".modal_menu");
 
-for (let i = 0; i < menuPosition.length; i++) {
-  const modalCard = document.createElement("div");
-  modalCard.classList.add("modal_card");
-  modalCard.id = menuPosition[i].ID;
+// for (let i = 0; i < menuPosition.length; i++) {
+//   const modalCard = document.createElement("div");
+//   modalCard.classList.add("modal_card");
+//   modalCard.id = menuPosition[i].ID;
 
-  modalCard.innerHTML = `<div class="img_modal_card">
-    <img src="${menuPosition[i].photo}" alt="" />
-  </div>
-  <div class="description_modal_card">
-    <div class="structure_modal_card">${menuPosition[i].name}, <br> ${menuPosition[i].base} </div>
-    <div class="weight_modal_card">${menuPosition[i].weight} грамм, ⌀ ${menuPosition[i].diameter}</div>
-    <div class="compound_modal_card">${menuPosition[i].ingredients}</div>
-    <div class="price_modal_card">${menuPosition[i].price}₽</div>
-  </div>`;
+//   modalCard.innerHTML = `<div class="img_modal_card">
+//     <img src="${menuPosition[i].photo}" alt="" />
+//   </div>
+//   <div class="description_modal_card">
+//     <div class="structure_modal_card">${menuPosition[i].name}, <br> ${menuPosition[i].base} </div>
+//     <div class="weight_modal_card">${menuPosition[i].weight} грамм, ⌀ ${menuPosition[i].diameter}</div>
+//     <div class="compound_modal_card">${menuPosition[i].ingredients}</div>
+//     <div class="price_modal_card">${menuPosition[i].price}₽</div>
+//   </div>`;
 
-  modalMenu.appendChild(modalCard);
-}
+//   modalMenu.appendChild(modalCard);
+// }
 
 
