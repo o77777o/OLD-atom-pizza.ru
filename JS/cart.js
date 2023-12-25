36113404289691cb32cf8401217369a11e85ad3e
o77777o <136020078+o77777o@users.noreply.github.com>
2023-12-25 16:57:22 +0300

    Update cart.js

--- JS/cart.js
+++ JS/cart.js
@@ -49,17 +49,19 @@
 //   cart.push(selectedProduct);
 //   localStorage.setItem(DB_NAME, JSON.stringify(cart));
 // };
+
 const DB_NAME = "DataBase";
 
 const clearAllCart = () => {
   window.localStorage.setItem(DB_NAME, JSON.stringify([]));
+  let productToDelete = document.querySelectorAll(".product")
+  productToDelete.forEach((element) => {
+    element.remove();
+  });
 };
 
 const processButton = (id) => {
   const element = menuPosition.find((element) => element.ID === id);
-  console.log(element.ID);
-  console.log(id)
-
 
   if (!element) {
     alert("Ты куда жмал бля");
@@ -67,10 +69,8 @@ const processButton = (id) => {
   }
 
   const all = JSON.parse(localStorage.getItem(DB_NAME));
-
   localStorage.setItem(DB_NAME, JSON.stringify([...all, element]));
 };
-
 
 const init = () => {
   if (!localStorage.getItem(DB_NAME)) {
@@ -81,12 +81,30 @@ const init = () => {
 
   for (const product of products) {
     const description = product.childNodes[2];
+
     const addToCartButton = description.children[2];
+    // console.log(addToCartButton);
 
     addToCartButton.addEventListener("click", () => {
       processButton(product.id);
+      getCartFromLS(element);
     });
   }
+};
+
+const getCartFromLS = (element) => {
+  let all = JSON.parse(localStorage.getItem(DB_NAME));
+  let totalProduct = document.querySelector(".total_product");
+
+  all.forEach((element) => {
+    const productInCart = document.createElement("div");
+    productInCart.classList.add("product");
+    productInCart.innerHTML = `
+      <div class="product_number">${all.length}.</div>
+      <div class="product_name">${all[element].name}</div>
+      `;
+    totalProduct.appendChild(productInCart);
+  } )
 };
 
 document.querySelector(".button_clear_all").addEventListener("click", () => {
@@ -96,3 +114,15 @@ document.querySelector(".button_clear_all").addEventListener("click", () => {
 window.onload = () => {
   init();
 };
+
+// let totalProduct = document.querySelector(".total_product");
+
+// const productInCart = document.createElement("div");
+// productInCart.classList.add("product");
+// productInCart.innerHTML = `
+//   <div class="product_number">${all.lenght + 1}.</div>
+//   <div class="product_name">${all[element].name + 1}</div>
+//   <div class="product_count">-настроить-</div>
+//   <div class="product_price">${all[element].price}₽</div>
+//   `;
+// totalProduct.appendChild(productInCart);
