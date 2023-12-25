8181a8f69fd5a1333f0df1f7af9bf1409705eb15
Mikel <136020078+o77777o@users.noreply.github.com>
2023-12-25 22:54:52 +0300

    upd

--- JS/cart.js
+++ JS/cart.js
@@ -1,75 +1,79 @@
-// let selectedProduct = "";
-// let productNumber = 0;
-
-// let cart = JSON.parse(localStorage.getItem(DB_NAME));
-
-// const allButtons = document.querySelectorAll(
-//   ".price_product_card"
-// );
-// const totalProduct = document.querySelector(".total_product");
-// //----------------------------------------------
-
-// const getCartFromLS = (i) => {
-//   productNumber += 1;
-
-//   const productInCart = document.createElement("div");
-//   productInCart.classList.add("product");
-//   productInCart.innerHTML = `
-//     <div class="product_number">${productNumber}.</div>
-//     <div class="product_name">${cart[i].name}</div>
-//     <div class="product_count">3 шт.</div>
-//     <div class="product_price">${cart[i].price}₽</div>
-//     `;
-//   totalProduct.appendChild(productInCart);
-// };
-
-// const displayProductToCart = (i) => {
-//   productNumber += 1;
-//   const correctIndex = i % menuPosition.length;
-//   const productInCart = document.createElement("div");
-//   productInCart.classList.add("product");
-//   productInCart.innerHTML = `
-//     <div class="product_number">${productNumber}.</div>
-//     <div class="product_name">${menuPosition[correctIndex].name}</div>
-//     <div class="product_count">3 шт.</div>
-//     <div class="product_price">${menuPosition[correctIndex].price}₽</div>
-//     `;
-//   totalProduct.appendChild(productInCart);
-// };
-
-// const saveProductToLS = (i) => {
-//   const correctIndex = i % menuPosition.length;
-//   console.log(menuPosition, menuPosition.length, correctIndex);
-//   selectedProduct = menuPosition[correctIndex];
-
-//   cart = JSON.parse(localStorage.getItem(DB_NAME));
-//   if (!localStorage.getItem(DB_NAME)) {
-//     localStorage.setItem(DB_NAME, JSON.stringify([]));
-//   }
-//   cart.push(selectedProduct);
-//   localStorage.setItem(DB_NAME, JSON.stringify(cart));
-// };
-
 const DB_NAME = "DataBase";
 
 const clearAllCart = () => {
   window.localStorage.setItem(DB_NAME, JSON.stringify([]));
-  let productToDelete = document.querySelectorAll(".product")
+  const productToDelete = document.querySelectorAll(".product");
   productToDelete.forEach((element) => {
     element.remove();
   });
 };
 
+const deleteProduct = (id) => {
+  const all = JSON.parse(localStorage.getItem(DB_NAME));
+  const elementHTML = productToDelete.find((product) => product.ID === id)
+  const elementLS = all.find((product) => product.ID === id)
+
+}
+
+
+
+
+
+const getAllCart = () => {
+  const all = JSON.parse(localStorage.getItem(DB_NAME));
+  let totalProduct = document.querySelector(".total_product");
+  for (let i = 0; i < all.length; i++) {
+    const productInCart = document.createElement("div");
+    productInCart.classList.add("product");
+    productInCart.id = all[i].ID
+    productInCart.innerHTML = `
+    <div class="product_number">${i + 1}.</div>
+    <div class="product_name">${all[i].name}</div>
+    <div class="product_count">1 шт.</div>
+    <div class="product_price">${all[i].price}₽</div>
+    <div class="delete_btn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <line x1="18" y1="6" x2="6" y2="18"/>
+    <line x1="6" y1="6" x2="18" y2="18"/>
+  </svg></div>
+    `;
+    totalProduct.appendChild(productInCart);
+  }
+};
+
 const processButton = (id) => {
-  const element = menuPosition.find((element) => element.ID === id);
+  const element = menuPosition.find((position) => position.ID === id);
+  if (!element) {
+    alert("Ты куда жмал бля");
+    return;
+  }
+  const all = JSON.parse(localStorage.getItem(DB_NAME));
+  localStorage.setItem(DB_NAME, JSON.stringify([...all, element]));
+};
+
+const displayProduct = (id) => {
+  const element = menuPosition.find((position) => position.ID === id);
 
   if (!element) {
     alert("Ты куда жмал бля");
     return;
   }
+  let all = JSON.parse(localStorage.getItem(DB_NAME));
+  let totalProduct = document.querySelector(".total_product");
+  const productInCart = document.createElement("div");
+  productInCart.classList.add("product");
+  productInCart.id = element.ID
+  productInCart.innerHTML = `
+  <div class="product_number">${all.length}.</div>
+  <div class="product_name">${element.name}</div>
+  <div class="product_count"> 1 шт.</div>
+  <div class="product_price">${element.price}₽</div>
+  <div class="delete_btn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+  <line x1="18" y1="6" x2="6" y2="18"/>
+  <line x1="6" y1="6" x2="18" y2="18"/>
+</svg></div>
+  `;
 
-  const all = JSON.parse(localStorage.getItem(DB_NAME));
-  localStorage.setItem(DB_NAME, JSON.stringify([...all, element]));
+  totalProduct.appendChild(productInCart);
 };
 
 const init = () => {
@@ -87,24 +91,9 @@ const init = () => {
 
     addToCartButton.addEventListener("click", () => {
       processButton(product.id);
-      getCartFromLS(element);
+      displayProduct(product.id);
     });
   }
-};
-
-const getCartFromLS = (element) => {
-  let all = JSON.parse(localStorage.getItem(DB_NAME));
-  let totalProduct = document.querySelector(".total_product");
-
-  all.forEach((element) => {
-    const productInCart = document.createElement("div");
-    productInCart.classList.add("product");
-    productInCart.innerHTML = `
-      <div class="product_number">${all.length}.</div>
-      <div class="product_name">${all[element].name}</div>
-      `;
-    totalProduct.appendChild(productInCart);
-  } )
 };
 
 document.querySelector(".button_clear_all").addEventListener("click", () => {
@@ -112,17 +101,6 @@ document.querySelector(".button_clear_all").addEventListener("click", () => {
 });
 
 window.onload = () => {
+  getAllCart();
   init();
 };
-
-// let totalProduct = document.querySelector(".total_product");
-
-// const productInCart = document.createElement("div");
-// productInCart.classList.add("product");
-// productInCart.innerHTML = `
-//   <div class="product_number">${all.lenght + 1}.</div>
-//   <div class="product_name">${all[element].name + 1}</div>
-//   <div class="product_count">-настроить-</div>
-//   <div class="product_price">${all[element].price}₽</div>
-//   `;
-// totalProduct.appendChild(productInCart);
--- JS/черновик.js
+++ JS/черновик.js
@@ -54,3 +54,54 @@
 // }
 
 //---------------------------------------------------------------------
+// let selectedProduct = "";
+// let productNumber = 0;
+
+// let cart = JSON.parse(localStorage.getItem(DB_NAME));
+
+// const allButtons = document.querySelectorAll(
+//   ".price_product_card"
+// );
+// const totalProduct = document.querySelector(".total_product");
+// //----------------------------------------------
+
+// const getCartFromLS = (i) => {
+//   productNumber += 1;
+
+//   const productInCart = document.createElement("div");
+//   productInCart.classList.add("product");
+//   productInCart.innerHTML = `
+//     <div class="product_number">${productNumber}.</div>
+//     <div class="product_name">${cart[i].name}</div>
+//     <div class="product_count">3 шт.</div>
+//     <div class="product_price">${cart[i].price}₽</div>
+//     `;
+//   totalProduct.appendChild(productInCart);
+// };
+
+// const displayProductToCart = (i) => {
+//   productNumber += 1;
+//   const correctIndex = i % menuPosition.length;
+//   const productInCart = document.createElement("div");
+//   productInCart.classList.add("product");
+//   productInCart.innerHTML = `
+//     <div class="product_number">${productNumber}.</div>
+//     <div class="product_name">${menuPosition[correctIndex].name}</div>
+//     <div class="product_count">3 шт.</div>
+//     <div class="product_price">${menuPosition[correctIndex].price}₽</div>
+//     `;
+//   totalProduct.appendChild(productInCart);
+// };
+
+// const saveProductToLS = (i) => {
+//   const correctIndex = i % menuPosition.length;
+//   console.log(menuPosition, menuPosition.length, correctIndex);
+//   selectedProduct = menuPosition[correctIndex];
+
+//   cart = JSON.parse(localStorage.getItem(DB_NAME));
+//   if (!localStorage.getItem(DB_NAME)) {
+//     localStorage.setItem(DB_NAME, JSON.stringify([]));
+//   }
+//   cart.push(selectedProduct);
+//   localStorage.setItem(DB_NAME, JSON.stringify(cart));
+// };
