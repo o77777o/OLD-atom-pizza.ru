7332bf91ad29c01e8208e437ae8b80c4c668faf3
Mikel <136020078+o77777o@users.noreply.github.com>
2023-12-28 02:30:48 +0300

    upd

--- JS/cart.js
+++ JS/cart.js
@@ -98,13 +98,13 @@ const createElement = (source) => {
   product.classList.add("product");
   product.id = source.ID;
   product.innerHTML = `
-  
-  <div class="product_name">${source.name}</div>
+  <div class="product_name">• ${source.type} «${source.name}»</div>
   <div class="product_price">${source.price}₽</div>
-  <div class="delete_btn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-  <line x1="18" y1="6" x2="6" y2="18"/>
-  <line x1="6" y1="6" x2="18" y2="18"/>
+  <div class="delete_btn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3.4" stroke-linecap="round" stroke-linejoin="round">
+  <line x1="18" y1="6" x2="6" y2="18" stroke="white"/>
+  <line x1="6" y1="6" x2="18" y2="18" stroke="white"/>
   </svg></div>
+
   `;
   return product;
 };
